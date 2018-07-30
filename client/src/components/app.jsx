8cd0e4e987282dbdc6eb0d41c761fbcd39e26966
Raymond Cabrera <rjgcengr@gmail.com>
2018-07-30 15:31:01 -0700

    Remove .gitkeep files from src folder, and center h1 and tr elems

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>CryptoChart</h1>
+        <h1>Top 10 Cryptocurrencies by Market Capitalization</h1>
         <CryptoList />
       </div>
     )
